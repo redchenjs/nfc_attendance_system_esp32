3fc373e6a2a94d9cd2df2582c5b0669837d195df
redchenjs <redchenjs@live.com>
2018-03-10 15:46:23 +0800

    increase i2s dma buf count

--- main/src/tasks/main_task.c
+++ main/src/tasks/main_task.c
@@ -24,10 +24,10 @@ void main_task(void)
 {
     task_event_group = xEventGroupCreate();
     system_event_group = xEventGroupCreate();
-    xTaskCreate(&mp3_player_task, "mp3_player_task", 8192, NULL, 5, NULL);
-    xTaskCreate(&sntp_client_task, "sntp_client_task", 2048, NULL, 5, NULL);
-    xTaskCreate(&oled_display_task, "oled_display_task", 2048, NULL, 5, NULL);
-    xTaskCreate(&led_indicator_task, "led_indicator_task", 1024, NULL, 5, NULL);
-    xTaskCreate(&nfc_initiator_task, "nfc_initiator_task", 5120, NULL, 5, NULL);
-    xTaskCreate(&token_verifier_task, "token_verifier_task", 19200, NULL, 5, NULL);
+    xTaskCreate(mp3_player_task, "mp3_player_task", 8192, NULL, 5, NULL);
+    xTaskCreate(sntp_client_task, "sntp_client_task", 2048, NULL, 5, NULL);
+    xTaskCreate(oled_display_task, "oled_display_task", 2048, NULL, 5, NULL);
+    xTaskCreate(led_indicator_task, "led_indicator_task", 1024, NULL, 5, NULL);
+    xTaskCreate(nfc_initiator_task, "nfc_initiator_task", 5120, NULL, 5, NULL);
+    xTaskCreate(token_verifier_task, "token_verifier_task", 19200, NULL, 5, NULL);
 }
