eaaf85d5b60b3e6cdad5adbf1fdfe6cf4f47ffd4
redchenjs <redchenjs@live.com>
2018-03-19 11:06:43 +0800

    st7735: fix a typo

--- main/src/driver/st7735.c
+++ main/src/driver/st7735.c
@@ -86,7 +86,7 @@ void st7735_refresh_gram(uint8_t *gram)
     spi1_trans[3].tx_data[0] = 0x00;    // starty high byte
     spi1_trans[3].tx_data[1] = 0x1A;    // starty low byte
     spi1_trans[3].tx_data[2] = 0x00;    // endy high byte
-    spi1_trans[3].tx_data[3] = 0x69;    // endy high byte
+    spi1_trans[3].tx_data[3] = 0x69;    // endy low byte
     spi1_trans[3].user = (void*)1;
     spi1_trans[3].flags = SPI_TRANS_USE_TXDATA;
 
