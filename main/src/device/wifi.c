94833ab56e8bc7e9cb041ef9bb932e10134f3a8d
redchenjs <redchenjs@live.com>
2018-04-01 21:51:48 +0800

    wifi: save wifi config to nvs flash

--- main/src/device/wifi.c
+++ main/src/device/wifi.c
@@ -21,23 +21,38 @@ char wifi0_mac_str[18] = {0};
 
 void wifi0_init(void)
 {
-    tcpip_adapter_init();
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-    ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
     wifi_config_t wifi_config = {
         .sta = {
-            .ssid = DEFAULT_SSID,
-            .password = DEFAULT_PASSWORD,
             .scan_method = WIFI_ALL_CHANNEL_SCAN,
             .sort_method = WIFI_CONNECT_AP_BY_SIGNAL,
             .threshold.rssi = -127,
             .threshold.authmode = WIFI_AUTH_WPA2_PSK
         },
     };
-    ESP_LOGI(TAG, "setting wifi configuration, ssid: %s", wifi_config.sta.ssid);
+    tcpip_adapter_init();
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+    ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_FLASH));
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
-    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
+    wifi_config_t wifi_stored_config;
+    ESP_ERROR_CHECK(esp_wifi_get_config(ESP_IF_WIFI_STA, &wifi_stored_config));
+    if (strlen((char *)wifi_stored_config.sta.ssid) != 0) {
+        ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_stored_config));
+    } else if (strlen(CONFIG_WIFI_SSID) != 0) {
+        strncpy(
+            (char *)wifi_config.sta.ssid,
+            (char *)CONFIG_WIFI_SSID,
+            strlen(CONFIG_WIFI_SSID)
+        );
+        wifi_config.sta.ssid[strlen(CONFIG_WIFI_SSID)] = '\0';
+        strncpy(
+            (char *)wifi_config.sta.password,
+            (char *)CONFIG_WIFI_PASSWORD,
+            strlen(CONFIG_WIFI_PASSWORD)
+        );
+        wifi_config.sta.password[strlen(CONFIG_WIFI_PASSWORD)] = '\0';
+        ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
+    }
     ESP_ERROR_CHECK(esp_wifi_start());
     uint8_t wifi0_mac[6] = {0};
     ESP_ERROR_CHECK(esp_wifi_get_mac(ESP_IF_WIFI_STA, wifi0_mac));
