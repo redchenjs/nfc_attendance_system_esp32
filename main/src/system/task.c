5971935b7da5817bd7586e17c0d7712af2290f75
redchenjs <redchenjs@live.com>
2018-04-01 21:56:58 +0800

    system: add wifi_daemon task and update task stack size

--- main/src/system/task.c
+++ main/src/system/task.c
@@ -8,9 +8,10 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
-#include "tasks/gui_task.h"
+#include "tasks/gui_daemon.h"
 #include "tasks/mp3_player.h"
 #include "tasks/sntp_client.h"
+#include "tasks/wifi_daemon.h"
 #include "tasks/blufi_daemon.h"
 #include "tasks/led_indicator.h"
 #include "tasks/nfc_initiator.h"
@@ -19,12 +20,13 @@
 void task_init(void)
 {
 #if defined(CONFIG_ENABLE_GUI)
-    xTaskCreate(gui_task, "gui_task", 2048, NULL, 5, NULL);
+    xTaskCreate(gui_daemon_task, "gui_daemon_task", 1920, NULL, 5, NULL);
 #endif
 #if defined(CONFIG_ENABLE_VOICE_PROMPT)
     xTaskCreate(mp3_player_task, "mp3_player_task", 8192, NULL, 5, NULL);
 #endif
-    xTaskCreate(sntp_client_task, "sntp_client_task", 2048, NULL, 5, NULL);
+    xTaskCreate(sntp_client_task, "sntp_client_task", 1920, NULL, 5, NULL);
+    xTaskCreate(wifi_daemon_task, "wifi_daemon_task", 1920, NULL, 5, NULL);
 #if defined(CONFIG_ENABLE_BLUFI)
     xTaskCreate(blufi_daemon_task, "blufi_daemon_task", 1024, NULL, 5, NULL);
 #endif
