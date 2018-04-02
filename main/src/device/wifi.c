e398693202f33993603754afd2da7c24b1cfdd37
redchenjs <redchenjs@live.com>
2018-04-02 14:46:04 +0800

    wifi: add BLUFI & WIFI store config

--- main/src/device/wifi.c
+++ main/src/device/wifi.c
@@ -7,13 +7,14 @@
 
 #include <string.h>
 
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+
 #include "esp_log.h"
 #include "esp_wifi.h"
 
-/*Set the SSID and Password via "make menuconfig"*/
-#define DEFAULT_SSID CONFIG_WIFI_SSID
-#define DEFAULT_PASSWORD CONFIG_WIFI_PASSWORD
-#define DEFAULT_HOSTNAME CONFIG_WIFI_HOSTNAME
+#include "system/event.h"
 
 char wifi0_mac_str[18] = {0};
 
@@ -32,13 +33,21 @@ void wifi0_init(void)
     tcpip_adapter_init();
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-    ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_FLASH));
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+#if defined(CONFIG_ENABLE_BLUFI) && defined(CONFIG_STORE_WIFI_TO_NVS)
+    ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_FLASH));
     wifi_config_t wifi_stored_config;
     ESP_ERROR_CHECK(esp_wifi_get_config(ESP_IF_WIFI_STA, &wifi_stored_config));
     if (strlen((char *)wifi_stored_config.sta.ssid) != 0) {
+        ESP_LOGI(TAG, "use stored wifi configuration, ssid: %s", wifi_stored_config.sta.ssid);
         ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_stored_config));
+        ESP_ERROR_CHECK(esp_wifi_start());
+        ESP_ERROR_CHECK(tcpip_adapter_set_hostname(TCPIP_ADAPTER_IF_STA, CONFIG_WIFI_HOSTNAME));
     } else if (strlen(CONFIG_WIFI_SSID) != 0) {
+#else
+    ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+    if (strlen(CONFIG_WIFI_SSID) != 0) {
+#endif
         strncpy(
             (char *)wifi_config.sta.ssid,
             (char *)CONFIG_WIFI_SSID,
@@ -51,11 +60,15 @@ void wifi0_init(void)
             strlen(CONFIG_WIFI_PASSWORD)
         );
         wifi_config.sta.password[strlen(CONFIG_WIFI_PASSWORD)] = '\0';
+        ESP_LOGI(TAG, "use default wifi configuration, ssid: %s", wifi_config.sta.ssid);
         ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
+        ESP_ERROR_CHECK(esp_wifi_start());
+        ESP_ERROR_CHECK(tcpip_adapter_set_hostname(TCPIP_ADAPTER_IF_STA, CONFIG_WIFI_HOSTNAME));
+    } else {
+        ESP_LOGW(TAG, "no wifi configuration available");
+        xEventGroupSetBits(system_event_group, WIFI_NO_CONFIG_BIT);
     }
-    ESP_ERROR_CHECK(esp_wifi_start());
     uint8_t wifi0_mac[6] = {0};
     ESP_ERROR_CHECK(esp_wifi_get_mac(ESP_IF_WIFI_STA, wifi0_mac));
     snprintf(wifi0_mac_str, sizeof(wifi0_mac_str), MACSTR, MAC2STR(wifi0_mac));
-    ESP_ERROR_CHECK(tcpip_adapter_set_hostname(TCPIP_ADAPTER_IF_STA, CONFIG_WIFI_HOSTNAME));
 }
