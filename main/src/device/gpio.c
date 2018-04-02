e93f0cbd3c8936f095f26c07bb538c778f359cc9
redchenjs <redchenjs@live.com>
2018-04-02 14:43:17 +0800

    gpio: add CONFIG_ENABLE_LED

--- main/src/device/gpio.c
+++ main/src/device/gpio.c
@@ -7,19 +7,19 @@
 
 #include "driver/gpio.h"
 
-#define LED1_PIN 25
-
-#define GPIO_OUTPUT_PIN_SEL (1ULL<<LED1_PIN)
+#define LED1_PIN CONFIG_LED_PIN_NUM
 
 void gpio0_init(void)
 {
+#if defined(CONFIG_ENABLE_LED)
     gpio_config_t io_conf = {
         .intr_type = GPIO_PIN_INTR_DISABLE,
         .mode = GPIO_MODE_OUTPUT,
-        .pin_bit_mask = GPIO_OUTPUT_PIN_SEL,
+        .pin_bit_mask = 1ULL << LED1_PIN,
         .pull_down_en = 0,
         .pull_up_en = 0
     };
 
     gpio_config(&io_conf);
+#endif
 }
