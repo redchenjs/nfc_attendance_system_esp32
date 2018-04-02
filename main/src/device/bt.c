1153108b567ac396f261b52387639e227ee1d945
redchenjs <redchenjs@live.com>
2018-04-02 14:48:04 +0800

    bt: add CONFIG_ENABLE_BLUFI config

--- main/src/device/bt.c
+++ main/src/device/bt.c
@@ -20,6 +20,7 @@ char bt0_addr_str[18] = {0};
 
 void bt0_init(void)
 {
+#if defined(CONFIG_ENABLE_BLUFI)
     ESP_ERROR_CHECK(esp_bt_controller_mem_release(ESP_BT_MODE_CLASSIC_BT));
     esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_bt_controller_init(&bt_cfg));
@@ -27,13 +28,16 @@ void bt0_init(void)
     ESP_ERROR_CHECK(esp_bluedroid_init());
     ESP_ERROR_CHECK(esp_bluedroid_enable());
     snprintf(bt0_addr_str, sizeof(bt0_addr_str), ""ESP_BD_ADDR_STR"", ESP_BD_ADDR_HEX(esp_bt_dev_get_address()));
+#endif
 }
 
 void bt0_deinit(void)
 {
+#if defined(CONFIG_ENABLE_BLUFI)
     ESP_ERROR_CHECK(esp_bluedroid_disable());
     ESP_ERROR_CHECK(esp_bluedroid_deinit());
     ESP_ERROR_CHECK(esp_bt_controller_disable());
     ESP_ERROR_CHECK(esp_bt_controller_deinit());
+#endif
     ESP_ERROR_CHECK(esp_bt_controller_mem_release(ESP_BT_MODE_BTDM));
 }
