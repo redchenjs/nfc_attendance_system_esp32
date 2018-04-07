ecec18b41050c4f07ea06861043fbca1be349f21
redchenjs <redchenjs@live.com>
2018-04-07 21:40:12 +0800

    task: update task name

--- main/src/system/task.c
+++ main/src/system/task.c
@@ -9,30 +9,34 @@
 #include "freertos/task.h"
 
 #include "tasks/gui_daemon.h"
-#include "tasks/mp3_player.h"
-#include "tasks/sntp_client.h"
+#include "tasks/ntp_daemon.h"
+#include "tasks/led_daemon.h"
+#include "tasks/nfc_daemon.h"
 #include "tasks/wifi_daemon.h"
+#include "tasks/audio_daemon.h"
 #include "tasks/blufi_daemon.h"
-#include "tasks/led_indicator.h"
-#include "tasks/nfc_initiator.h"
-#include "tasks/token_verifier.h"
+#include "tasks/http2_daemon.h"
 
 void task_init(void)
 {
-#if defined(CONFIG_ENABLE_GUI)
-    xTaskCreate(gui_daemon_task, "gui_daemon_task", 1920, NULL, 5, NULL);
-#endif
-#if defined(CONFIG_ENABLE_VOICE_PROMPT)
-    xTaskCreate(mp3_player_task, "mp3_player_task", 8192, NULL, 5, NULL);
-#endif
-    xTaskCreate(sntp_client_task, "sntp_client_task", 1920, NULL, 5, NULL);
-    xTaskCreate(wifi_daemon_task, "wifi_daemon_task", 1920, NULL, 5, NULL);
-#if defined(CONFIG_ENABLE_BLUFI)
-    xTaskCreate(blufi_daemon_task, "blufi_daemon_task", 1024, NULL, 5, NULL);
-#endif
-    xTaskCreate(nfc_initiator_task, "nfc_initiator_task", 5120, NULL, 5, NULL);
 #if defined(CONFIG_ENABLE_LED)
-    xTaskCreate(led_indicator_task, "led_indicator_task", 1024, NULL, 5, NULL);
+    xTaskCreate(led_daemon, "led_daemon", 1024, NULL, 6, NULL);
 #endif
-    xTaskCreate(token_verifier_task, "token_verifier_task", 19200, NULL, 5, NULL);
+
+#if defined(CONFIG_ENABLE_GUI)
+    xTaskCreate(gui_daemon, "gui_daemon", 1920, NULL, 6, NULL);
+#endif
+
+#if defined(CONFIG_ENABLE_AUDIO)
+    xTaskCreate(audio_daemon, "audio_daemon", 8192, NULL, 6, NULL);
+#endif
+
+#if defined(CONFIG_ENABLE_BLUFI)
+    xTaskCreate(blufi_daemon, "blufi_daemon", 1024, NULL, 6, NULL);
+#endif
+
+    xTaskCreate(nfc_daemon, "nfc_daemon", 5120, NULL, 4, NULL);
+    xTaskCreate(ntp_daemon, "ntp_daemon", 1920, NULL, 5, NULL);
+    xTaskCreate(wifi_daemon, "wifi_daemon", 1920, NULL, 5, NULL);
+    xTaskCreate(http2_daemon, "http2_daemon", 19200, NULL, 5, NULL);
 }
