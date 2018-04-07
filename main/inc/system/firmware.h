e33c8cb858a8140ff19eef621af8ad6f59ae056e
redchenjs <redchenjs@live.com>
2018-04-07 21:01:39 +0800

    system: add firmware get version function

---
+++ main/inc/system/firmware.h
@@ -0,0 +1,13 @@
+/*
+ * firmware.h
+ *
+ *  Created on: 2018-04-05 19:09
+ *      Author: Jack Chen <redchenjs@live.com>
+ */
+
+#ifndef INC_SYSTEM_FIRMWARE_H_
+#define INC_SYSTEM_FIRMWARE_H_
+
+extern const char *firmware_get_version(void);
+
+#endif /* INC_SYSTEM_FIRMWARE_H_ */
