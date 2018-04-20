18a28097a5640944212bb7d7bdda871d645dd5ac
redchenjs <redchenjs@live.com>
2018-04-20 19:25:08 +0800

    task: update audio_daemon and ntp_daemon task stack size

--- main/src/system/task.c
+++ main/src/system/task.c
@@ -28,7 +28,7 @@ void task_init(void)
 #endif
 
 #if defined(CONFIG_ENABLE_AUDIO)
-    xTaskCreate(audio_daemon, "audio_daemon", 8192, NULL, 6, NULL);
+    xTaskCreate(audio_daemon, "audio_daemon", 8448, NULL, 6, NULL);
 #endif
 
 #if defined(CONFIG_ENABLE_BLUFI)
@@ -36,7 +36,7 @@ void task_init(void)
 #endif
 
     xTaskCreate(nfc_daemon, "nfc_daemon", 5120, NULL, 4, NULL);
-    xTaskCreate(ntp_daemon, "ntp_daemon", 1920, NULL, 5, NULL);
+    xTaskCreate(ntp_daemon, "ntp_daemon", 2048, NULL, 5, NULL);
     xTaskCreate(wifi_daemon, "wifi_daemon", 1920, NULL, 5, NULL);
     xTaskCreate(http2_daemon, "http2_daemon", 19200, NULL, 5, NULL);
 }
