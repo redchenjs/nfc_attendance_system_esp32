74a21d4959953b153e0b1843428688a9a109e7d4
redchenjs <redchenjs@live.com>
2018-04-28 21:06:43 +0800

    task: update task priority

--- main/src/system/task.c
+++ main/src/system/task.c
@@ -19,6 +19,10 @@
 
 void task_init(void)
 {
+    xTaskCreate(nfc_daemon, "nfc_daemon", 4000, NULL, 5, NULL);
+    xTaskCreate(ntp_daemon, "ntp_daemon", 2000, NULL, 5, NULL);
+    xTaskCreate(wifi_daemon, "wifi_daemon", 768, NULL, 5, NULL);
+
 #if defined(CONFIG_ENABLE_LED)
     xTaskCreate(led_daemon, "led_daemon", 640, NULL, 6, NULL);
 #endif
@@ -28,15 +32,12 @@ void task_init(void)
 #endif
 
 #if defined(CONFIG_ENABLE_AUDIO)
-    xTaskCreate(audio_daemon, "audio_daemon", 8448, NULL, 6, NULL);
+    xTaskCreate(audio_daemon, "audio_daemon", 8448, NULL, 7, NULL);
 #endif
 
 #if defined(CONFIG_ENABLE_BLUFI)
-    xTaskCreate(blufi_daemon, "blufi_daemon", 960, NULL, 6, NULL);
+    xTaskCreate(blufi_daemon, "blufi_daemon", 960, NULL, 7, NULL);
 #endif
 
-    xTaskCreate(nfc_daemon, "nfc_daemon", 4000, NULL, 4, NULL);
-    xTaskCreate(ntp_daemon, "ntp_daemon", 2000, NULL, 5, NULL);
-    xTaskCreate(wifi_daemon, "wifi_daemon", 768, NULL, 5, NULL);
-    xTaskCreate(http2_daemon, "http2_daemon", 19200, NULL, 5, NULL);
+    xTaskCreate(http2_daemon, "http2_daemon", 19200, NULL, 7, NULL);
 }
