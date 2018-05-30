f25563e3d782b14db07c5f7b8f832d9afd10711c
redchenjs <redchenjs@live.com>
2018-05-30 16:53:26 +0800

    bt: update driver

--- main/src/device/bt.c
+++ main/src/device/bt.c
@@ -14,28 +14,27 @@
 #include "esp_bt_device.h"
 #include "esp_log.h"
 
-char bt0_addr_str[18] = {0};
-
 #define TAG "bt-0"
 
 void bt0_init(void)
 {
+#if defined(CONFIG_ENABLE_BLUFI)
     ESP_ERROR_CHECK(esp_bt_controller_mem_release(ESP_BT_MODE_CLASSIC_BT));
     esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_bt_controller_init(&bt_cfg));
     ESP_ERROR_CHECK(esp_bt_controller_enable(ESP_BT_MODE_BLE));
     ESP_ERROR_CHECK(esp_bluedroid_init());
     ESP_ERROR_CHECK(esp_bluedroid_enable());
-    snprintf(bt0_addr_str, sizeof(bt0_addr_str), ""ESP_BD_ADDR_STR"", ESP_BD_ADDR_HEX(esp_bt_dev_get_address()));
+#else
+    ESP_ERROR_CHECK(esp_bt_controller_mem_release(ESP_BT_MODE_BTDM));
+#endif
 }
 
 void bt0_deinit(void)
 {
-#if defined(CONFIG_ENABLE_BLUFI)
     ESP_ERROR_CHECK(esp_bluedroid_disable());
     ESP_ERROR_CHECK(esp_bluedroid_deinit());
     ESP_ERROR_CHECK(esp_bt_controller_disable());
     ESP_ERROR_CHECK(esp_bt_controller_deinit());
-#endif
     ESP_ERROR_CHECK(esp_bt_controller_mem_release(ESP_BT_MODE_BTDM));
 }
