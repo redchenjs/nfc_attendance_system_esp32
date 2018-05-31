af03265e972633a5bb835cb4baf540a5ea85999a
redchenjs <redchenjs@live.com>
2018-05-31 20:18:27 +0800

    system/user: remove blufi_daemon & add key_daemon

--- main/src/system/user.c
+++ main/src/system/user.c
@@ -9,22 +9,24 @@
 #include "freertos/task.h"
 
 #include "user/gui_daemon.h"
+#include "user/key_daemon.h"
 #include "user/ntp_daemon.h"
 #include "user/led_daemon.h"
 #include "user/nfc_daemon.h"
-#include "user/wifi_daemon.h"
 #include "user/audio_daemon.h"
-#include "user/blufi_daemon.h"
 #include "user/http2_daemon.h"
 
 void user_init(void)
 {
-    xTaskCreate(nfc_daemon, "nfc_daemon", 4000, NULL, 5, NULL);
-    xTaskCreate(ntp_daemon, "ntp_daemon", 2000, NULL, 5, NULL);
-    xTaskCreate(wifi_daemon, "wifi_daemon", 768, NULL, 5, NULL);
+    xTaskCreate(nfc_daemon, "nfc_daemon", 5120, NULL, 5, NULL);
+    xTaskCreate(ntp_daemon, "ntp_daemon", 2048, NULL, 5, NULL);
+
+#if defined(CONFIG_ENABLE_SMART_CONFIG)
+    xTaskCreate(key_daemon, "key_daemon", 2048, NULL, 5, NULL);
+#endif
 
 #if defined(CONFIG_ENABLE_LED)
-    xTaskCreate(led_daemon, "led_daemon", 640, NULL, 6, NULL);
+    xTaskCreate(led_daemon, "led_daemon", 1024, NULL, 6, NULL);
 #endif
 
 #if defined(CONFIG_ENABLE_GUI)
@@ -33,10 +35,6 @@ void user_init(void)
 
 #if defined(CONFIG_ENABLE_AUDIO)
     xTaskCreate(audio_daemon, "audio_daemon", 8448, NULL, 7, NULL);
-#endif
-
-#if defined(CONFIG_ENABLE_BLUFI)
-    xTaskCreate(blufi_daemon, "blufi_daemon", 1024, NULL, 7, NULL);
 #endif
 
     xTaskCreate(http2_daemon, "http2_daemon", 19200, NULL, 7, NULL);
