e402c79f1668aa927c4e943bdc41c911cde67f95
redchenjs <redchenjs@live.com>
2018-06-08 14:57:36 +0800

    gui_daemon: update res struct

--- main/inc/user/gui_daemon.h
+++ main/inc/user/gui_daemon.h
@@ -12,60 +12,60 @@
 
 #if defined(CONFIG_SCREEN_PANEL_SSD1331)
 // ani0.gif
-extern const uint8_t ani0_96x64_gif_ptr[] asm("_binary_ani0_96x64_gif_start");
-extern const uint8_t ani0_96x64_gif_end[] asm("_binary_ani0_96x64_gif_end");
+extern const char ani0_96x64_gif_ptr[] asm("_binary_ani0_96x64_gif_start");
+extern const char ani0_96x64_gif_end[] asm("_binary_ani0_96x64_gif_end");
 // ani1.gif
-extern const uint8_t ani1_96x64_gif_ptr[] asm("_binary_ani1_96x64_gif_start");
-extern const uint8_t ani1_96x64_gif_end[] asm("_binary_ani1_96x64_gif_end");
+extern const char ani1_96x64_gif_ptr[] asm("_binary_ani1_96x64_gif_start");
+extern const char ani1_96x64_gif_end[] asm("_binary_ani1_96x64_gif_end");
 // ani2.gif
-extern const uint8_t ani2_96x64_gif_ptr[] asm("_binary_ani2_96x64_gif_start");
-extern const uint8_t ani2_96x64_gif_end[] asm("_binary_ani2_96x64_gif_end");
+extern const char ani2_96x64_gif_ptr[] asm("_binary_ani2_96x64_gif_start");
+extern const char ani2_96x64_gif_end[] asm("_binary_ani2_96x64_gif_end");
 // ani3.gif
-extern const uint8_t ani3_96x64_gif_ptr[] asm("_binary_ani3_96x64_gif_start");
-extern const uint8_t ani3_96x64_gif_end[] asm("_binary_ani3_96x64_gif_end");
+extern const char ani3_96x64_gif_ptr[] asm("_binary_ani3_96x64_gif_start");
+extern const char ani3_96x64_gif_end[] asm("_binary_ani3_96x64_gif_end");
 // ani4.gif
-extern const uint8_t ani4_96x64_gif_ptr[] asm("_binary_ani4_96x64_gif_start");
-extern const uint8_t ani4_96x64_gif_end[] asm("_binary_ani4_96x64_gif_end");
+extern const char ani4_96x64_gif_ptr[] asm("_binary_ani4_96x64_gif_start");
+extern const char ani4_96x64_gif_end[] asm("_binary_ani4_96x64_gif_end");
 // ani5.gif
-extern const uint8_t ani5_96x64_gif_ptr[] asm("_binary_ani5_96x64_gif_start");
-extern const uint8_t ani5_96x64_gif_end[] asm("_binary_ani5_96x64_gif_end");
+extern const char ani5_96x64_gif_ptr[] asm("_binary_ani5_96x64_gif_start");
+extern const char ani5_96x64_gif_end[] asm("_binary_ani5_96x64_gif_end");
 // ani6.gif
-extern const uint8_t ani6_96x64_gif_ptr[] asm("_binary_ani6_96x64_gif_start");
-extern const uint8_t ani6_96x64_gif_end[] asm("_binary_ani6_96x64_gif_end");
+extern const char ani6_96x64_gif_ptr[] asm("_binary_ani6_96x64_gif_start");
+extern const char ani6_96x64_gif_end[] asm("_binary_ani6_96x64_gif_end");
 // ani7.gif
-extern const uint8_t ani7_96x64_gif_ptr[] asm("_binary_ani7_96x64_gif_start");
-extern const uint8_t ani7_96x64_gif_end[] asm("_binary_ani7_96x64_gif_end");
+extern const char ani7_96x64_gif_ptr[] asm("_binary_ani7_96x64_gif_start");
+extern const char ani7_96x64_gif_end[] asm("_binary_ani7_96x64_gif_end");
 // ani8.gif
-extern const uint8_t ani8_96x64_gif_ptr[] asm("_binary_ani8_96x64_gif_start");
-extern const uint8_t ani8_96x64_gif_end[] asm("_binary_ani8_96x64_gif_end");
+extern const char ani8_96x64_gif_ptr[] asm("_binary_ani8_96x64_gif_start");
+extern const char ani8_96x64_gif_end[] asm("_binary_ani8_96x64_gif_end");
 #elif defined(CONFIG_SCREEN_PANEL_ST7735)
 // ani0.gif
-extern const uint8_t ani0_160x80_gif_ptr[] asm("_binary_ani0_160x80_gif_start");
-extern const uint8_t ani0_160x80_gif_end[] asm("_binary_ani0_160x80_gif_end");
+extern const char ani0_160x80_gif_ptr[] asm("_binary_ani0_160x80_gif_start");
+extern const char ani0_160x80_gif_end[] asm("_binary_ani0_160x80_gif_end");
 // ani1.gif
-extern const uint8_t ani1_160x80_gif_ptr[] asm("_binary_ani1_160x80_gif_start");
-extern const uint8_t ani1_160x80_gif_end[] asm("_binary_ani1_160x80_gif_end");
+extern const char ani1_160x80_gif_ptr[] asm("_binary_ani1_160x80_gif_start");
+extern const char ani1_160x80_gif_end[] asm("_binary_ani1_160x80_gif_end");
 // ani2.gif
-extern const uint8_t ani2_160x80_gif_ptr[] asm("_binary_ani2_160x80_gif_start");
-extern const uint8_t ani2_160x80_gif_end[] asm("_binary_ani2_160x80_gif_end");
+extern const char ani2_160x80_gif_ptr[] asm("_binary_ani2_160x80_gif_start");
+extern const char ani2_160x80_gif_end[] asm("_binary_ani2_160x80_gif_end");
 // ani3.gif
-extern const uint8_t ani3_160x80_gif_ptr[] asm("_binary_ani3_160x80_gif_start");
-extern const uint8_t ani3_160x80_gif_end[] asm("_binary_ani3_160x80_gif_end");
+extern const char ani3_160x80_gif_ptr[] asm("_binary_ani3_160x80_gif_start");
+extern const char ani3_160x80_gif_end[] asm("_binary_ani3_160x80_gif_end");
 // ani4.gif
-extern const uint8_t ani4_160x80_gif_ptr[] asm("_binary_ani4_160x80_gif_start");
-extern const uint8_t ani4_160x80_gif_end[] asm("_binary_ani4_160x80_gif_end");
+extern const char ani4_160x80_gif_ptr[] asm("_binary_ani4_160x80_gif_start");
+extern const char ani4_160x80_gif_end[] asm("_binary_ani4_160x80_gif_end");
 // ani5.gif
-extern const uint8_t ani5_160x80_gif_ptr[] asm("_binary_ani5_160x80_gif_start");
-extern const uint8_t ani5_160x80_gif_end[] asm("_binary_ani5_160x80_gif_end");
+extern const char ani5_160x80_gif_ptr[] asm("_binary_ani5_160x80_gif_start");
+extern const char ani5_160x80_gif_end[] asm("_binary_ani5_160x80_gif_end");
 // ani6.gif
-extern const uint8_t ani6_160x80_gif_ptr[] asm("_binary_ani6_160x80_gif_start");
-extern const uint8_t ani6_160x80_gif_end[] asm("_binary_ani6_160x80_gif_end");
+extern const char ani6_160x80_gif_ptr[] asm("_binary_ani6_160x80_gif_start");
+extern const char ani6_160x80_gif_end[] asm("_binary_ani6_160x80_gif_end");
 // ani7.gif
-extern const uint8_t ani7_160x80_gif_ptr[] asm("_binary_ani7_160x80_gif_start");
-extern const uint8_t ani7_160x80_gif_end[] asm("_binary_ani7_160x80_gif_end");
+extern const char ani7_160x80_gif_ptr[] asm("_binary_ani7_160x80_gif_start");
+extern const char ani7_160x80_gif_end[] asm("_binary_ani7_160x80_gif_end");
 // ani8.gif
-extern const uint8_t ani8_160x80_gif_ptr[] asm("_binary_ani8_160x80_gif_start");
-extern const uint8_t ani8_160x80_gif_end[] asm("_binary_ani8_160x80_gif_end");
+extern const char ani8_160x80_gif_ptr[] asm("_binary_ani8_160x80_gif_start");
+extern const char ani8_160x80_gif_end[] asm("_binary_ani8_160x80_gif_end");
 #endif
 
 extern void gui_daemon(void *pvParameter);
--- main/src/user/gui_daemon.c
+++ main/src/user/gui_daemon.c
@@ -13,29 +13,29 @@
 
 #define TAG "gui"
 
-static const uint8_t *img_file_ptr[][2] = {
+static const char *img_file_ptr[][2] = {
 #if defined(CONFIG_SCREEN_PANEL_SSD1331)
-                                            {ani0_96x64_gif_ptr, ani0_96x64_gif_end}, // "WiFi"
-                                            {ani1_96x64_gif_ptr, ani1_96x64_gif_end}, // "Loading"
-                                            {ani2_96x64_gif_ptr, ani2_96x64_gif_end}, // "Success"
-                                            {ani3_96x64_gif_ptr, ani3_96x64_gif_end}, // "NFC"
-                                            {ani4_96x64_gif_ptr, ani4_96x64_gif_end}, // "PowerOff"
-                                            {ani5_96x64_gif_ptr, ani5_96x64_gif_end}, // "Clock"
-                                            {ani6_96x64_gif_ptr, ani6_96x64_gif_end}, // "Error"
-                                            {ani7_96x64_gif_ptr, ani7_96x64_gif_end}, // "Config"
-                                            {ani8_96x64_gif_ptr, ani8_96x64_gif_end}  // "Updating"
+    {ani0_96x64_gif_ptr, ani0_96x64_gif_end}, // "WiFi"
+    {ani1_96x64_gif_ptr, ani1_96x64_gif_end}, // "Loading"
+    {ani2_96x64_gif_ptr, ani2_96x64_gif_end}, // "Success"
+    {ani3_96x64_gif_ptr, ani3_96x64_gif_end}, // "NFC"
+    {ani4_96x64_gif_ptr, ani4_96x64_gif_end}, // "PowerOff"
+    {ani5_96x64_gif_ptr, ani5_96x64_gif_end}, // "Clock"
+    {ani6_96x64_gif_ptr, ani6_96x64_gif_end}, // "Error"
+    {ani7_96x64_gif_ptr, ani7_96x64_gif_end}, // "Config"
+    {ani8_96x64_gif_ptr, ani8_96x64_gif_end}  // "Updating"
 #elif defined(CONFIG_SCREEN_PANEL_ST7735)
-                                            {ani0_160x80_gif_ptr, ani0_160x80_gif_end}, // "WiFi"
-                                            {ani1_160x80_gif_ptr, ani1_160x80_gif_end}, // "Loading"
-                                            {ani2_160x80_gif_ptr, ani2_160x80_gif_end}, // "Success"
-                                            {ani3_160x80_gif_ptr, ani3_160x80_gif_end}, // "NFC"
-                                            {ani4_160x80_gif_ptr, ani4_160x80_gif_end}, // "PowerOff"
-                                            {ani5_160x80_gif_ptr, ani5_160x80_gif_end}, // "Clock"
-                                            {ani6_160x80_gif_ptr, ani6_160x80_gif_end}, // "Error"
-                                            {ani7_160x80_gif_ptr, ani7_160x80_gif_end}, // "Config"
-                                            {ani8_160x80_gif_ptr, ani8_160x80_gif_end}  // "Updating"
+    {ani0_160x80_gif_ptr, ani0_160x80_gif_end}, // "WiFi"
+    {ani1_160x80_gif_ptr, ani1_160x80_gif_end}, // "Loading"
+    {ani2_160x80_gif_ptr, ani2_160x80_gif_end}, // "Success"
+    {ani3_160x80_gif_ptr, ani3_160x80_gif_end}, // "NFC"
+    {ani4_160x80_gif_ptr, ani4_160x80_gif_end}, // "PowerOff"
+    {ani5_160x80_gif_ptr, ani5_160x80_gif_end}, // "Clock"
+    {ani6_160x80_gif_ptr, ani6_160x80_gif_end}, // "Error"
+    {ani7_160x80_gif_ptr, ani7_160x80_gif_end}, // "Config"
+    {ani8_160x80_gif_ptr, ani8_160x80_gif_end}  // "Updating"
 #endif
-                                        };
+};
 static uint8_t img_file_index = 0;
 
 void gui_daemon(void *pvParameter)
