bbbaa7506c35bdc7752d142ce5ac53b397cb0828
redchenjs <redchenjs@live.com>
2018-06-08 15:01:04 +0800

    token_verify: use esp_http to simplify the code struct

--- main/inc/user/token_verify.h
+++ main/inc/user/token_verify.h
@@ -8,11 +8,10 @@
 #ifndef INC_USER_TOKEN_VERIFY_H_
 #define INC_USER_TOKEN_VERIFY_H_
 
-#include "user/http2_client.h"
+#include "esp_http_client.h"
 
-extern int token_verify_parse_data(struct http2c_handle *handle, const char *data, size_t len, int flags);
-extern int token_verify_prepare_data(struct http2c_handle *handle, char *buf, size_t length, uint32_t *data_flags);
-
+extern esp_err_t token_verify_event_handler(esp_http_client_event_t *evt);
+extern void token_verify_prepare_data(char *buf, int len);
 extern void token_verify(char *token);
 
 #endif /* INC_USER_TOKEN_VERIFY_H_ */
--- main/src/user/token_verify.c
+++ main/src/user/token_verify.c
@@ -10,6 +10,7 @@
 #include "cJSON.h"
 #include "esp_log.h"
 #include "esp_ota_ops.h"
+#include "esp_http_client.h"
 
 #include "device/wifi.h"
 #include "system/event.h"
@@ -18,55 +19,68 @@
 #include "user/nfc_daemon.h"
 #include "user/led_daemon.h"
 #include "user/audio_daemon.h"
-#include "user/http2_daemon.h"
-#include "user/http2_client.h"
 
 #define TAG "token"
 
 static char *data_ptr = NULL;
 
-int token_verify_parse_data(struct http2c_handle *handle, const char *data, size_t len, int flags)
+esp_err_t token_verify_event_handler(esp_http_client_event_t *evt)
 {
-    cJSON *root   = NULL;
-    cJSON *status = NULL;
-    if (len) {
-        root = cJSON_Parse(data);
-        if (cJSON_HasObjectItem(root, "status")) {
-            status = cJSON_GetObjectItemCaseSensitive(root, "status");
-            if (cJSON_IsTrue(status)) {
-                ESP_LOGW(TAG, "authentication success");
-                gui_show_image(2);
-                audio_play_file(1);
+    switch (evt->event_id) {
+    case HTTP_EVENT_ERROR:
+        break;
+    case HTTP_EVENT_ON_CONNECTED:
+        break;
+    case HTTP_EVENT_HEADER_SENT:
+        break;
+    case HTTP_EVENT_ON_HEADER:
+        break;
+    case HTTP_EVENT_ON_DATA: {
+        cJSON *root   = NULL;
+        cJSON *status = NULL;
+        if (evt->data_len) {
+            root = cJSON_Parse(evt->data);
+            if (cJSON_HasObjectItem(root, "status")) {
+                status = cJSON_GetObjectItemCaseSensitive(root, "status");
+                if (cJSON_IsTrue(status)) {
+                    ESP_LOGW(TAG, "authentication success");
+                    gui_show_image(2);
+                    audio_play_file(1);
+                } else {
+                    ESP_LOGE(TAG, "authentication failed");
+                    gui_show_image(6);
+                    audio_play_file(2);
+                }
             } else {
-                ESP_LOGE(TAG, "authentication failed");
+                ESP_LOGE(TAG, "invalid response");
                 gui_show_image(6);
-                audio_play_file(2);
+                audio_play_file(6);
+                xEventGroupSetBits(daemon_event_group, HTTP_DAEMON_TOKEN_FAILED_BIT);
             }
-        } else {
-            ESP_LOGE(TAG, "invalid response");
-            gui_show_image(6);
-            audio_play_file(6);
-            xEventGroupSetBits(daemon_event_group, HTTP2_DAEMON_TOKEN_FAILED_BIT);
+            cJSON_Delete(root);
         }
-        cJSON_Delete(root);
+        break;
     }
-    if (flags == DATA_RECV_RST_STREAM) {
-        xEventGroupClearBits(daemon_event_group, HTTP2_DAEMON_TOKEN_READY_BIT);
+    case HTTP_EVENT_ON_FINISH:
+        xEventGroupClearBits(daemon_event_group, HTTP_DAEMON_TOKEN_READY_BIT);
+        break;
+    case HTTP_EVENT_DISCONNECTED:
+        break;
+    default:
+        break;
     }
-    return 0;
+    return ESP_OK;
 }
 
-int token_verify_prepare_data(struct http2c_handle *handle, char *buf, size_t length, uint32_t *data_flags)
+void token_verify_prepare_data(char *buf, int len)
 {
     cJSON *root = NULL;
     root = cJSON_CreateObject();
     cJSON_AddNumberToObject(root, "request", 100);
     cJSON_AddStringToObject(root, "token", data_ptr);
     cJSON_AddStringToObject(root, "mac", wifi0_mac_str);
-    cJSON_PrintPreallocated(root, buf, length, 0);
+    cJSON_PrintPreallocated(root, buf, len, 0);
     cJSON_Delete(root);
-    (*data_flags) |= NGHTTP2_DATA_FLAG_EOF;
-    return strlen(buf);
 }
 
 void token_verify(char *token)
@@ -75,12 +89,12 @@ void token_verify(char *token)
     data_ptr = token;
     EventBits_t uxBits = xEventGroupSync(
         daemon_event_group,
-        HTTP2_DAEMON_TOKEN_READY_BIT,
-        HTTP2_DAEMON_TOKEN_FINISH_BIT,
+        HTTP_DAEMON_TOKEN_READY_BIT,
+        HTTP_DAEMON_TOKEN_FINISH_BIT,
         30000 / portTICK_RATE_MS
     );
-    if ((uxBits & HTTP2_DAEMON_TOKEN_FINISH_BIT) == 0) {
-        xEventGroupClearBits(daemon_event_group, HTTP2_DAEMON_TOKEN_READY_BIT);
+    if ((uxBits & HTTP_DAEMON_TOKEN_FINISH_BIT) == 0) {
+        xEventGroupClearBits(daemon_event_group, HTTP_DAEMON_TOKEN_READY_BIT);
     }
     xEventGroupSetBits(system_event_group, INPUT_READY_BIT);
 }
