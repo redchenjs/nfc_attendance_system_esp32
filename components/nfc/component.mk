723dc636df50e0f65ba3150f7fbae3ff07dfde6b
redchenjs <redchenjs@live.com>
2019-04-21 17:53:30 +0800

    nfc: separate public & private include path

--- components/nfc/component.mk
+++ components/nfc/component.mk
@@ -1,2 +1,3 @@
 COMPONENT_SRCDIRS := nfc nfc/buses nfc/chips nfc/drivers
-COMPONENT_ADD_INCLUDEDIRS := include nfc nfc/buses nfc/chips nfc/drivers
+COMPONENT_ADD_INCLUDEDIRS := include
+COMPONENT_PRIV_INCLUDEDIRS := nfc nfc/buses nfc/chips nfc/drivers
