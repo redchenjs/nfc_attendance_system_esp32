48bdeea202e0a77a96c2121594677693f48fefd3
redchenjs <redchenjs@live.com>
2019-04-26 10:56:58 +0800

    build: remove Makefile build system

--- components/nfc/component.mk
+++
@@ -1,5 +0,0 @@
-CPPFLAGS := -DHAVE_CONFIG_H
-
-COMPONENT_SRCDIRS := nfc nfc/buses nfc/chips nfc/drivers
-COMPONENT_ADD_INCLUDEDIRS := include
-COMPONENT_PRIV_INCLUDEDIRS := nfc nfc/buses nfc/chips nfc/drivers
--- main/component.mk
+++
@@ -1,6 +0,0 @@
-COMPONENT_SRCDIRS := src src/user src/system src/driver src/device
-COMPONENT_ADD_INCLUDEDIRS := inc
-COMPONENT_EMBED_TXTFILES := res/cert/cert0.pem
-COMPONENT_EMBED_FILES := res/snd/snd0.mp3 res/snd/snd1.mp3 res/snd/snd2.mp3 res/snd/snd3.mp3 res/snd/snd4.mp3 res/snd/snd5.mp3 res/snd/snd6.mp3 res/snd/snd7.mp3
-COMPONENT_EMBED_FILES += res/ani/ani0_96x64.gif res/ani/ani1_96x64.gif res/ani/ani2_96x64.gif res/ani/ani3_96x64.gif res/ani/ani4_96x64.gif res/ani/ani5_96x64.gif res/ani/ani6_96x64.gif res/ani/ani7_96x64.gif res/ani/ani8_96x64.gif
-COMPONENT_EMBED_FILES += res/ani/ani0_160x80.gif res/ani/ani1_160x80.gif res/ani/ani2_160x80.gif res/ani/ani3_160x80.gif res/ani/ani4_160x80.gif res/ani/ani5_160x80.gif res/ani/ani6_160x80.gif res/ani/ani7_160x80.gif res/ani/ani8_160x80.gif
