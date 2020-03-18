3d727571bed8ab9fe486d25356df695474b11734
Jack Chen <redchenjs@live.com>
2020-03-18 17:36:27 +0800

    audio_render: code clean up

--- main/src/user/audio_render.c
+++ main/src/user/audio_render.c
@@ -16,8 +16,8 @@
 void render_sample_block(short *sample_buff_ch0, short *sample_buff_ch1, int num_samples, unsigned int num_channels)
 {
     // pointer to left / right sample position
-    char *ptr_l = (char*)sample_buff_ch0;
-    char *ptr_r = (char*)sample_buff_ch1;
+    char *ptr_l = (char *)sample_buff_ch0;
+    char *ptr_r = (char *)sample_buff_ch1;
     uint8_t stride = sizeof(short);
 
     if (num_channels == 1) {
@@ -27,20 +27,16 @@ void render_sample_block(short *sample_buff_ch0, short *sample_buff_ch1, int num
     size_t bytes_written = 0;
     for (int i = 0; i < num_samples; i++) {
         /* low - high / low - high */
-        const char samp32[4] = {ptr_l[0], ptr_l[1], ptr_r[0], ptr_r[1]}; // ESP32 CPU is Little Endian
+        const char samp32[4] = {ptr_l[0], ptr_l[1], ptr_r[0], ptr_r[1]}; // ESP32 CPU is little-endian
+
         i2s_write(CONFIG_AUDIO_OUTPUT_I2S_NUM, (const char *)&samp32, sizeof(samp32), &bytes_written, portMAX_DELAY);
 
-        // DMA buffer full - retry
-        if (bytes_written == 0) {
-            i--;
-        } else {
-            ptr_l += stride;
-            ptr_r += stride;
-        }
+        ptr_l += stride;
+        ptr_r += stride;
     }
 }
 
-/* Called by the NXP modifications of libmad. Sets the needed output sample rate. */
+/* frame callback for the libmad synth, set the needed output sample rate */
 void set_dac_sample_rate(int rate)
 {
     i2s_output_set_sample_rate(rate);
