2308301a782f932261c9f08acf367f646a029a6d
Jack Chen <redchenjs@live.com>
2020-11-07 14:04:20 +0800

    board: code cleanup

--- main/src/board/pn532.c
+++ main/src/board/pn532.c
@@ -23,7 +23,7 @@ void pn532_init(void)
         .mode = GPIO_MODE_OUTPUT,
         .pull_up_en = false,
         .pull_down_en = false,
-        .intr_type = GPIO_INTR_DISABLE,
+        .intr_type = GPIO_INTR_DISABLE
     };
     gpio_config(&io_conf);
 
